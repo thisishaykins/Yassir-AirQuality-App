0ad913891d8119cae295f580f0fc15044bfff7ab
Samuel AKINDE <samuel.akinde@cassbana.com>
2023-04-14 08:15:37 +0100

    TypeORM configs for DB configurations, entities configs, and migrations

---
+++ src/typeorm.config.ts
@@ -0,0 +1,24 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: +process.env.POSTGRES_PORT,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  entities: [__dirname + '/**/*.entity.ts', __dirname + '/**/*.entity.js'],
+  migrationsRun: true,
+  logging: true,
+  migrationsTableName: 'migration',
+  migrations: [
+    __dirname + '/migration/**/*.ts',
+    __dirname + '/migration/**/*.js',
+  ],
+  synchronize: true,
+  //   cli: {
+  //     migrationsDir: 'src/migration',
+  //   },
+};
+
+export = typeOrmConfig;
