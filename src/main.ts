ecf53f89a235144f83f9dd4734182771a7ed9407
Samuel AKINDE <samuel.akinde@cassbana.com>
2023-04-14 08:16:09 +0100

    Swagger Docume API configuration...

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = +process.env.APP_PORT || 3000;
+  // app.setGlobalPrefix('api');
+
+  const options = new DocumentBuilder()
+    // .addBearerAuth()
+    .setTitle('Air Quality App')
+    .setDescription('Air Quality API Documentation')
+    .setVersion('0.0.1')
+    .addTag('AirQuality')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(port);
 }
 bootstrap();
